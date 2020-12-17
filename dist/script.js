e4c4eedc03330b54c1490c65124b22eff8b5281d
dkhole <dkhlee419@gmail.com>
2020-12-17 15:40:23 +1100

    first commit

---
+++ dist/script.js
@@ -0,0 +1,12 @@
+/*const greeting: string = 'hello'
+
+console.log(greeting);
+
+
+const projects: HTMLCollectionOf<any> = document.getElementsByClassName('project');
+
+for(let i: number = 0; i < projects.length; i++) {
+    const proj: HTMLElement = projects[i];
+}
+
+console.log(projects); */ 
